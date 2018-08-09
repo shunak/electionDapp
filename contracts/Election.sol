929309ed9a8188aaa2da43543aa3f6947a5dc74c
DESKTOP-BRIGC4V\USER <shuna966@gmail.com>
2018-08-09 23:27:33 +0900

    180809

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,18 @@
 pragma solidity ^0.4.24;
-
+ 
 contract Election {
-    // Store candidate
-    // Read candidate
-    string public candidate;
-    //   Constructor 
-    // function Election() public {
-    constructor() public{
-        candidate = "Candidate 1";
+    // Model a CAndidate
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
     }
+    // Store candidate
+    // Fetch candidatei
+    mapping(uint => Candidate) public candidates; // when we add candidate to thsi ampping we are changing the state of our contract
+    // Store candidates Count
 
+    constructor() public{
+
+    }
 }
