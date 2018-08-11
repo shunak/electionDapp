6a320a58e73f67591dc635d5deabd04489dd07db
DESKTOP-BRIGC4V\USER <shuna966@gmail.com>
2018-08-11 18:52:20 +0900

    180811

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -11,8 +11,18 @@ contract Election {
     // Fetch candidatei
     mapping(uint => Candidate) public candidates; // when we add candidate to this mapping we are changing the state of our contract
     // Store candidates Count
+    uint public candidatesCount;
+
 
     constructor() public{
-
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
     }
+
+    // add Candidate
+    function addCandidate(string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount]=Candidate(candidatesCount,_name,0);
+    }
+
 }
